0cc4cb3ac2008226ea87ed0a2acad01ac6e3a522
hanuman12511 <er.hanumansaini@gmail.com>
2024-06-28 16:18:27 +0530

    ddd

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const multer = require('multer')
 
 const csvtojson = require('csvtojson')
 const reader = require('xlsx') 
-connect.Connection("test")
+connect.Connection("studentmgmt_smt")
 app.use('/', routes);
 const port = 4000
 /* const options = {key:"",cert:""};
